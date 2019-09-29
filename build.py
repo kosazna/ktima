387625e4991a2b38e747555d48df9f5d78a402da
aznavouridis.k <kosazna>
2019-09-29 13:59:05 +0300

    updates

--- build.py
+++ build.py
@@ -8,13 +8,14 @@
 # ---------------------------------------------------#
 from win.update import *
 
-meleti = (raw_input("\nMeleti\n").upper())
+meleti = (raw_input("\n\nMeleti\n\n").upper())
 
 kt_info_path = cp([meleti, inputdata, docs_i, 'KT_Info.json'])
 
 
 def build_structure():
-    repo = cp([mdev, 'Diafora', 'logs', 'scripts', 'Folder_Structure'], origin=ktl['temp'][user])
+    # repo = cp([mdev, 'Diafora', 'logs', 'scripts', 'Folder_Structure'], origin=ktl['temp'][user])
+    repo = r"C:\Users\aznavouridis.k\Desktop\repo\Folder_Structure"
     target = r"C:\{}".format(meleti)
 
     try:
@@ -23,9 +24,29 @@ def build_structure():
         print('"{}" directory already exists'.format(target))
 
 
-def update_structure():
-    repo = cp([mdev, 'Diafora', 'logs', 'scripts', 'File_Structure'], origin=ktl['temp'][user])
+def update_basic_structure():
+    # repo = cp([mdev, 'Diafora', 'logs', 'scripts', 'Folder_Structure'], origin=ktl['temp'][user])
+    repo = r"C:\Users\aznavouridis.k\Desktop\repo\Folder_Structure"
+    for dirpath, dirnames, filenames in os.walk(repo):
+        outpath = [meleti] + dirpath.split('\\')[6:]
+        _dir = cp(outpath)
+        if not os.path.exists(_dir):
+            os.makedirs(_dir)
+            print('{} - Created'.format(_dir))
 
+
+def update_folder_structure():
+    # repo = cp([mdev, 'Diafora', 'logs', 'scripts', 'File_Structure', meleti], origin=ktl['temp'][user])
+    repo = r"C:\Users\aznavouridis.k\Desktop\repo\File_Structure\{}".format(meleti)
+    for dirpath, dirnames, filenames in os.walk(repo):
+        outpath = cp(dirpath.split('\\')[6:])
+        if not os.path.exists(outpath):
+            os.makedirs(outpath)
+
+
+def update_file_structure():
+    # repo = cp([mdev, 'Diafora', 'logs', 'scripts', 'File_Structure', meleti], origin=ktl['temp'][user])
+    repo = r"C:\Users\aznavouridis.k\Desktop\repo\File_Structure\{}".format(meleti)
     for dirpath, dirnames, filenames in os.walk(repo):
         for filename in filenames:
             inpath = os.path.join(dirpath, filename)
