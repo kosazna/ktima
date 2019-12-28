908668955b838fcdb16efb7009e41f94a278085d
aznavouridis.k <kosazna>
2019-12-28 18:50:34 +0200

    Updates

--- testing.py
+++ testing.py
@@ -7,17 +7,25 @@ import shutil
 import getpass
 import sys
 
+
+def count_lines():
+    files = Files(cp(['Python27', 'ArcGIS10.1', 'Lib', 'site-packages', 'ktima']))
+    files.list_files(match='.py')
+    c = 0
+    for i in files.filepaths:
+        count = len(open(i).readlines())
+        c += count
+    print(c)
+
+
 ##################################################
 meleti = 'KT2-11'
 company_name = 'NAMA'
 mel_type = 1
-
 paths = Paths(meleti, mel_type, company_name)
 kt_map = load_json(paths.kt_info_path)
 kt = NamesAndLists(kt_map)
-
 ##################################################
-
 
 # def all_files():
 #     src = cp(['Python27', 'ArcGIS10.1', 'Lib', 'site-packages', 'ktima'])
@@ -31,15 +39,3 @@ kt = NamesAndLists(kt_map)
 #     else:
 #         print("\nWARNING! - Directories don't match\n")
 #
-#
-# all_files()
-
-files = Files(cp(['Python27', 'ArcGIS10.1', 'Lib', 'site-packages', 'ktima']))
-files.list_files(match='.py')
-
-c = 0
-
-for i in files.filepaths:
-    count = len(open(i).readlines())
-    c += count
-print(c)
