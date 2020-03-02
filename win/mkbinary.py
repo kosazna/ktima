c01950d2411aefd3ffd611c5dcc217a242e830bd
kos_azna <kaznavouridis@gmail.com>
2020-03-02 10:51:19 +0200

    Added documantation.

--- win/mkbinary.py
+++ win/mkbinary.py
@@ -6,13 +6,26 @@
 #                                                    #
 #             aznavouridis.k@gmail.com               #
 # ---------------------------------------------------#
+
+# Module is being used to compile python code to pyc files
+# so that it can be delivered to the production
+
 import py_compile
 from ktima.handler import *
 from update import toolboxes
 
 
 def compile_ktima():
+    """
+    Compiles the entire codebase except from some files.
+
+    :return: Nothing
+    """
+
+    # FILES TO NOT BE COMPILED
     no_compile = ['testing', 'pass', 'mkbinary']
+
+    # SOURCE CODE PATH
     src = cp([users, user, 'Desktop', 'compiled code'])
 
     for fullpath, filename, basename, ext in list_dir(src, match='.py'):
